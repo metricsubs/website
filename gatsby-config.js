535b787b11115bf14d21ac6028ead80090d493da
Dizy <dev@dizy.cc>
2021-05-13 19:13:51 +0800

    Switch to TypeScript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: "MetricSubs",
+    title: 'MetricSubs',
   },
   plugins: [
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-mdx",
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-mdx',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
   ],
 };
